b908aa9b9e18298984673e677b31da8f9d4794bb
Techphonic <techphonic@gmail.com>
2016-03-14 03:32:28 -0400

    initial commit

---
+++ plot1.R
@@ -0,0 +1,10 @@
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+dataFile <- unz(temp,"household_power_consumption.txt")
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+unlink(temp)
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
